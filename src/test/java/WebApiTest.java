fdfe83c1cca9937d40d403b2249d1c04eb1e6230
yujiorama <yujiorama+github@gmail.com>
2015-12-12 20:33:18 +0900

    add assertj/add example1

--- src/test/java/WebApiTest.java
+++ src/test/java/WebApiTest.java
@@ -1,9 +1,38 @@
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.parsing.Parser;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.assertj.core.api.Assertions.*;
+import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static com.jayway.restassured.module.jsv.JsonSchemaValidator.*;
+import static org.hamcrest.Matchers.*;
 
 public class WebApiTest {
-    @Test public void testSomeLibraryMethod() {
+    @Test
+    public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        assertThat(classUnderTest.someLibraryMethod()).isEqualTo(true);
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        RestAssured.registerParser("text/json", Parser.JSON);
+    }
+
+    @Test
+    public void example1() {
+        get("/lotto.json")
+        .then()
+        .body("lotto.lottoId", equalTo(5));
+    }
+
+    @Test
+    public void example1Alternative() {
+        get("/lotto.json")
+        .then()
+        .body("lotto.winners.winnerId", hasItems(23, 54));
     }
 }
